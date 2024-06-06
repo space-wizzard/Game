620fd434222a55c86da8299bfd260dcbd671346a
space-wizzard <141222454+space-wizzard@users.noreply.github.com>
2024-06-06 16:46:15 +0900

    술래잡기

--- GameObject.java
+++ GameObject.java
@@ -1,18 +1,38 @@
-package gameProject;
+package inheritance;
 
 import java.awt.Image;
-import java.awt.event.ActionEvent;
-import java.util.LinkedList;
 
-public interface GameObject {
-	int[][] direction = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-    static int END = 17;
-    int[][] maze = new int[END][END];
-   boolean[][] visited = new boolean[END][END];
-   int[][] parent = new int[END * END][2];
-   LinkedList<int[]> path = new LinkedList<>();
-   Image[][] panel = new Image[END][END];
-   GameObject[][] gamePane=new GameObject[END][END];
-   int[][] ItemPosition= new int[END][END];
-   
+import javax.swing.JCheckBox;
+// 곰과 물고기 객체에 필요한 메서드와 변수 지정
+public abstract class GameObject {
+	protected int distance;
+	protected int x,y;
+	int end_x=10;
+	int end_y=20;
+	protected JCheckBox[] direction= new JCheckBox[4];
+	// 위 오른쪽 왼쪽 아래
+	protected String[] names= {"up","right","left","down"};
+	public GameObject(int startX,int startY,int distance) {
+		this.x = startX;
+		this.y = startY;
+		this.distance = distance;
+
+	}
+	
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	protected abstract void move(String key);
+	protected abstract Image getShape();
+	protected abstract void limit();
+	
 }
+
+
+
+
